5d60243301ed47485a777a82e5a0d8ebc3d29b6e
Александр Хрусталев <returnXAA@outlook.com>
2020-12-30 18:57:58 +0500

    Добавьте файлы проекта.

---
+++ MobileToDo/MobileToDo/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileToDo
+{
+	[DesignTimeVisible(true)]
+	public class MainViewModel : INotifyPropertyChanged
+	{
+
+		public MainViewModel()
+		{
+			_text = "";
+			_lableText = "";
+			_tasks = new ObservableCollection<Task>();
+		}
+
+		private string _text;
+		private string _lableText;
+		private ObservableCollection<Task> _tasks;
+		
+		public ObservableCollection<Task> Tasks
+		{
+			get => _tasks;
+			set
+			{
+				_tasks = value;
+				OnPropertyChanged(nameof(Tasks));
+			}
+		}
+
+		public string LabelText
+		{
+			get => _lableText;
+			set
+			{
+				_lableText = value;
+				OnPropertyChanged(nameof(LabelText));
+			}
+		}
+
+		public string Text
+		{
+			get => _text;
+			set
+			{
+				_text = value;
+				OnPropertyChanged(nameof(Text));
+			}
+		}
+		
+		private ICommand _buttonCommand;
+		private ICommand _changechekCommand;
+		
+		public ICommand ButtonCommand => _buttonCommand ?? (_buttonCommand = new RelayCommand(ButtonCommandExe));
+		public ICommand ChangechekCommand => _changechekCommand ?? (_changechekCommand = new RelayCommand(ChangechekCommandExe));
+
+
+		
+		private void ButtonCommandExe()
+		{
+			Task T = new Task();
+			T.TextTask = Text;
+			T.Id = Tasks.Count;
+			Tasks.Add(T);
+			Text = "";
+		}
+		private void ChangechekCommandExe()
+		{
+			throw new NotImplementedException();
+		}
+
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged == null)
+				return;
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ MobileToDo/MobileToDo/Task.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+
+
+namespace MobileToDo
+{
+	public class Task
+	{
+		public Task()
+		{
+			_id = 0;
+			_check = false;
+			_textTask = "";
+		}
+		private int _id;
+		private bool _check;
+		private string _textTask;
+
+		public int Id
+		{
+			get => _id;
+			set
+			{
+				_id = value;
+				OnPropertyChanged(nameof(Id));
+			}
+		}
+
+		public bool Check
+		{
+			get => _check;
+			set
+			{
+				_check = value;
+				OnPropertyChanged(nameof(Check));
+			}
+		}
+
+		public string TextTask
+		{
+			get => _textTask;
+			set
+			{
+				_textTask = value;
+				OnPropertyChanged(nameof(TextTask));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged == null)
+				return;
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+}
